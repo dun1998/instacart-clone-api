0405c78139cd903a5d71eb67c015de47ea3ac555
Duncan MacMurchy <duncanmacmurchy@gmail.com>
2025-06-17 20:52:22 -0700

    test: add failing test to validate CreateGroceryCompanyItem with duplicate data returns null

--- tests/GroceryApi.Tests/UnitTests/GroceryCompanyItemServiceTests.cs
+++ tests/GroceryApi.Tests/UnitTests/GroceryCompanyItemServiceTests.cs
@@ -48,4 +48,45 @@ public class GroceryCompanyItemServiceTests
         Assert.Equal(groceryCompanyItem.GroceryItemId, groceryItemId);
         Assert.Equal(groceryCompanyItem.Price, price);
     }
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData(1.0, 1.0)]
+    [InlineData(0.0, null)]
+    [InlineData(null, 0.0)]
+    [InlineData(double.MaxValue, 0.0)]
+    public async Task CreateGroceryCompanyItem_WithDuplicateData_ReturnsNull(double? price, double? price2)
+    {
+        var context = UnitTestUtil.CreatInMemoryDbContext();
+        ILogger<GroceryCompanyItemService> logger = new Mock<ILogger<GroceryCompanyItemService>>().Object;
+
+        var companyId = 1;
+        var groceryItemId = 1;
+
+        GroceryCompany company = new GroceryCompany()
+        {
+            Id = companyId,
+            Name = "Grocery Company",
+        };
+        await context.GroceryCompanies.AddAsync(company);
+
+        GroceryItem groceryItem = new GroceryItem()
+        {
+            Id = groceryItemId,
+            Name = "Grocery Item",
+            CategoryId = null,
+            Description = "A grocery item",
+            CreatedDate = DateTime.Now,
+            ModifiedDate = DateTime.Now
+        };
+        await context.GroceryItems.AddAsync(groceryItem);
+        await context.SaveChangesAsync();
+        var service = new GroceryCompanyItemService(logger, context);
+
+        GroceryCompanyItem? companyItem = await service.CreateGroceryCompanyItemAsync(companyId, groceryItemId, price);
+        Assert.NotNull(companyItem);
+        GroceryCompanyItem? companyItemDupe =
+            await service.CreateGroceryCompanyItemAsync(companyId, groceryItemId, price2);
+        Assert.Null(companyItemDupe);
+    }
 }
