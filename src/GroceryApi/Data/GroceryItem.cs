e73c05e4fab42ab97f4ae0b38a1e20faae22a013
Duncan MacMurchy <duncanmacmurchy@gmail.com>
2025-06-17 22:12:22 -0700

    refactor: altered GroceryItem to contain CreatedDate and Modified date along with max length strings, moved price to GroceryCompanyItem

--- src/GroceryApi/Data/GroceryItem.cs
+++ src/GroceryApi/Data/GroceryItem.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GroceryApi.Data;
 
 public class GroceryItem
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public decimal Price { get; set; }
-    public string Description { get; set; } = null!;
+
+    [MaxLength(255)] public string Name { get; set; } = null!;
+
+    [MaxLength(5000)] public string Description { get; set; } = null!;
+
     public int? CategoryId { get; set; } = null;
     public GroceryCategory? Category { get; set; } = null;
-    
-
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
 }
